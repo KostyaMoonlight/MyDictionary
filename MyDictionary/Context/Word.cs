7495f9b42d81aa7f1e0cbe26604eb4d2d81395d0
KostyaMoonlight <kostya9919moonlight@gmail.com>
2017-03-26 14:39:20 +0300

    Add Db and Entity

---
+++ MyDictionary/Context/Word.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary.Context
+{
+    [Table("Dictionary")]
+    public class Word
+    {
+        [Column("Id")]
+        [Key]
+        public int Id { get; set; }
+
+        [Column("Original")]
+        public string Original { get; set; }
+
+        [Column("Translation")]
+        public string Translation { get; set; }
+
+        [Column("Rating")]
+        public int Rating { get; set; }
+
+        [Column("Note")]
+        public string Note { get; set; }
+    }
+}
